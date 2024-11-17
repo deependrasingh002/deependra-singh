8c6baa93e1a6a807bd34399b909652f55eb84bb4
Deependra Singh <singhdepu566@gmail.com>
2024-11-17 22:29:37 +0530

    change in for mobile text fnont size

--- src/components/component/Title.jsx
+++ src/components/component/Title.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Title({number,title}) {
   return (
-    <div className="flex flex-row items-center gap-x-5 text-2xl">
-        <span className="text-primary">0{number}.</span>
-        <span className="text-secondary font-bold md:text-3xl whitespace-nowrap">
+    <div className="flex flex-row items-center gap-x-5 text-xl">
+        <span className="text-primary text-sm md:text-lg">0{number}.</span>
+        <span className="text-secondary font-bold   md:text-3xl whitespace-nowrap">
          {title}
         </span>
         <div className="after:content-['']  md:after:w-64 after:h-[2px] after:bg-secondaryLight after:block after:mt-2 mb-3"></div>
